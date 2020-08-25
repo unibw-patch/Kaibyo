79df886371bc84158ad4b75066eac1cae72f8752
Hernan Ponce de Leon <zeta96@gmail.com>
2020-08-25 17:17:55 +0200

    scripts

--- benchmarks/spectre/spectre-loop.c
+++ benchmarks/spectre/spectre-loop.c
@@ -11,6 +11,7 @@ extern void *__VERIFIER_nondet_pointer(void) ;
 #endif
 
 #define SIZE    (1)
+#define LOOP    (10)
 
 unsigned int array1_size = 16;
 uint8_t array1[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
