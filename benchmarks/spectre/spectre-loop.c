c46a286bc7c37dfe37d93eaa28802ebb7e59b998
Hernan Ponce de Leon <zeta96@gmail.com>
2020-08-25 17:17:08 +0200

    scripts

--- benchmarks/spectre/spectre-loop.c
+++ benchmarks/spectre/spectre-loop.c
@@ -11,7 +11,6 @@ extern void *__VERIFIER_nondet_pointer(void) ;
 #endif
 
 #define SIZE    (1)
-#define LOOP    (100)
 
 unsigned int array1_size = 16;
 uint8_t array1[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
@@ -26,9 +25,13 @@ uint8_t temp = 0; /* Used so compiler won’t optimize out victim_function() */
 // EXAMPLE 1:  This is the sample function from the Spectre paper.
 // ----------------------------------------------------------------------------------------
 void victim_function_v01(size_t x) {
-     if (x < array1_size) {
-          temp &= array2[array1[x] * SIZE];
-     }
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x < array1_size) {
+                temp &= array2[array1[x] * SIZE];
+            }
+        }
+    }
 }
 #endif
 
@@ -38,9 +41,13 @@ void victim_function_v01(size_t x) {
 // ----------------------------------------------------------------------------------------
 void leakByteLocalFunction(uint8_t k) { temp &= array2[(k)* SIZE]; }
 void victim_function_v02(size_t x) {
-     if (x < array1_size) {
-          leakByteLocalFunction(array1[x]);
-     }
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x < array1_size) {
+                leakByteLocalFunction(array1[x]);
+            }
+        }
+    }
 }
 #endif
 
@@ -53,8 +60,12 @@ void victim_function_v02(size_t x) {
 // ----------------------------------------------------------------------------------------
 __declspec(noinline) void leakByteNoinlineFunction(uint8_t k) { temp &= array2[(k)* SIZE]; }
 void victim_function_v03(size_t x) {
-     if (x < array1_size)
-          leakByteNoinlineFunction(array1[x]);
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x < array1_size)
+                leakByteNoinlineFunction(array1[x]);
+        }
+    }
 }
 #endif
 
@@ -65,8 +76,12 @@ void victim_function_v03(size_t x) {
 // Comments: Output is unsafe.
 // ----------------------------------------------------------------------------------------
 void victim_function_v04(size_t x) {
-     if (x < array1_size)
-          temp &= array2[array1[x << 1] * SIZE];
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x < array1_size)
+                temp &= array2[array1[x << 1] * SIZE];
+        }
+    }
 }
 #endif
 
@@ -78,11 +93,15 @@ void victim_function_v04(size_t x) {
 // ----------------------------------------------------------------------------------------
 void victim_function_v05(size_t x) {
      int i;
-     if (x < array1_size) {
-         for (i = x - 1; i >= 0; i--) {
-               temp &= array2[array1[i] * SIZE];
-         }
-     }
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x < array1_size) {
+                for (i = x - 1; i >= 0; i--) {
+                    temp &= array2[array1[i] * SIZE];
+                }
+            }
+        }
+    }
 }
 #endif
 
@@ -94,8 +113,12 @@ void victim_function_v05(size_t x) {
 // ----------------------------------------------------------------------------------------
 int array_size_mask = 15;
 void victim_function_v06(size_t x) {
-     if ((x & array_size_mask) == x)
-          temp &= array2[array1[x] * SIZE];
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if ((x & array_size_mask) == x)
+                temp &= array2[array1[x] * SIZE];
+        }
+    }
 }
 #endif
 
@@ -107,10 +130,14 @@ void victim_function_v06(size_t x) {
 // ----------------------------------------------------------------------------------------
 void victim_function_v07(size_t x) {
      static size_t last_x = 0;
-     if (x == last_x)
-          temp &= array2[array1[x] * SIZE];
-     if (x < array1_size)
-          last_x = x;
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x == last_x)
+                temp &= array2[array1[x] * SIZE];
+            if (x < array1_size)
+                last_x = x;
+        }
+    }
 }
 #endif
 
@@ -119,7 +146,11 @@ void victim_function_v07(size_t x) {
 // EXAMPLE 8:  Use a ?: operator to check bounds.
 // ----------------------------------------------------------------------------------------
 void victim_function_v08(size_t x) {
-     temp &= array2[array1[x < array1_size ? (x + 1) : 0] * SIZE];
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            temp &= array2[array1[x < array1_size ? (x + 1) : 0] * SIZE];
+        }
+    }
 }
 #endif
 
@@ -130,8 +161,12 @@ void victim_function_v08(size_t x) {
 // Comments: Output is unsafe.
 // ----------------------------------------------------------------------------------------
 void victim_function_v09(size_t x, int *x_is_safe) {
-     if (*x_is_safe)
-          temp &= array2[array1[x] * SIZE];
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (*x_is_safe)
+                temp &= array2[array1[x] * SIZE];
+        }
+    }
 }
 #endif
 
@@ -145,10 +180,14 @@ void victim_function_v09(size_t x, int *x_is_safe) {
 // values for k until finding the one that causes array2[0] to get brought into the cache.
 // ----------------------------------------------------------------------------------------
 void victim_function_v10(size_t x, uint8_t k) {
-     if (x < array1_size) {
-          if (array1[x] == k)
-               temp &= array2[0];
-     }
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x < array1_size) {
+                if (array1[x] == k)
+                    temp &= array2[0];
+            }
+        }
+    }
 }
 #endif
 
@@ -171,8 +210,12 @@ int mymemcmp(const void *cs, const void *ct, int count)
     return res;
 }
 void victim_function_v11(size_t x) {
-     if (x < array1_size)
-          temp = mymemcmp(&temp, array2 + (array1[x] * SIZE), 1);
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (x < array1_size)
+                temp = mymemcmp(&temp, array2 + (array1[x] * SIZE), 1);
+        }
+    }
 }
 #endif
 
@@ -201,8 +244,12 @@ int is_x_safe(size_t x) {
     return 0;
 }
 void victim_function_v13(size_t x) {
-     if (is_x_safe(x))
-          temp &= array2[array1[x] * SIZE];
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (is_x_safe(x))
+                temp &= array2[array1[x] * SIZE];
+        }
+    }
 }
 #endif
 
@@ -225,8 +272,12 @@ void victim_function_v14(size_t x) {
 // Comments: Output is unsafe.
 // ----------------------------------------------------------------------------------------
 void victim_function_v15(size_t *x) {
-     if (*x < array1_size)
-          temp &= array2[array1[*x] * SIZE];
+    for (int i = 1; i < LOOP; ++i) {
+        if(i == LOOP - 1) {
+            if (*x < array1_size)
+                temp &= array2[array1[*x] * SIZE];
+        }
+    }
 }
 #endif
 
@@ -238,61 +289,56 @@ int main()
     void *xp = __VERIFIER_nondet_pointer();
     uint8_t x8 = __VERIFIER_nondet_uchar();
     #else
-    // They won't be actually used
     size_t x;
     size_t y;
     void *xp;
     uint8_t x8;
     #endif
-
-    for (int i = 1; i < LOOP; ++i) {
-        if(i == LOOP - 1) {
-            #ifdef v01
-            victim_function_v01(x);
-            #endif
-            #ifdef v02
-            victim_function_v02(x);
-            #endif
-            #ifdef v03
-            victim_function_v03(x);
-            #endif
-            #ifdef v04
-            victim_function_v04(x);
-            #endif
-            #ifdef v05
-            victim_function_v05(x);
-            #endif
-            #ifdef v06
-            victim_function_v06(x);
-            #endif
-            #ifdef v07
-            victim_function_v07(x);
-            #endif
-            #ifdef v08
-            victim_function_v08(x);
-            #endif
-            #ifdef v09
-            victim_function_v09(x,xp);
-            #endif
-            #ifdef v10
-            victim_function_v10(x,x8);
-            #endif
-            #ifdef v11
-            victim_function_v11(x);
-            #endif
-            #ifdef v12
-            victim_function_v12(x,y);
-            #endif
-            #ifdef v13
-            victim_function_v13(x);
-            #endif
-            #ifdef v14
-            victim_function_v14(x);
-            #endif
-            #ifdef v15
-            victim_function_v15(xp);
-            #endif
-        }
-    }
+    
+    #ifdef v01
+    victim_function_v01(x);
+    #endif
+    #ifdef v02
+    victim_function_v02(x);
+    #endif
+    #ifdef v03
+    victim_function_v03(x);
+    #endif
+    #ifdef v04
+    victim_function_v04(x);
+    #endif
+    #ifdef v05
+    victim_function_v05(x);
+    #endif
+    #ifdef v06
+    victim_function_v06(x);
+    #endif
+    #ifdef v07
+    victim_function_v07(x);
+    #endif
+    #ifdef v08
+    victim_function_v08(x);
+    #endif
+    #ifdef v09
+    victim_function_v09(x,xp);
+    #endif
+    #ifdef v10
+    victim_function_v10(x,x8);
+    #endif
+    #ifdef v11
+    victim_function_v11(x);
+    #endif
+    #ifdef v12
+    victim_function_v12(x,y);
+    #endif
+    #ifdef v13
+    victim_function_v13(x);
+    #endif
+    #ifdef v14
+    victim_function_v14(x);
+    #endif
+    #ifdef v15
+    victim_function_v15(xp);
+    #endif
     return 0;
 }
