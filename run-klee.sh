7af1046beabba1bdab44a5e743b1a3186b189641
Hernan Ponce de Leon <zeta96@gmail.com>
2020-09-17 17:01:46 +0200

    script

--- run-klee.sh
+++ run-klee.sh
@@ -8,20 +8,20 @@ do
     echo =========================================================
     echo Running $version.none.o0.s
     log=output/logs/klee/$version.none.o0.log
-    timeout $timeout $whereisklee/build/bin/klee benchmarks/spectre/bc/$version.none.o0.bc --search=randomsp --enable-speculative 2> $log
+    timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative benchmarks/spectre/bc/$version.none.o0.bc 2> $log
     tail -n 1 "$log" | grep "KLEE: Total Spectre found"
     echo Running $version.none.o2.s
     log=output/logs/klee/$version.none.o2.log
-    timeout $timeout $whereisklee/build/bin/klee  benchmarks/spectre/bc/$version.none.o2.bc --search=randomsp --enable-speculative 2> $log
+    timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative benchmarks/spectre/bc/$version.none.o2.bc 2> $log
     tail -n 1 "$log" | grep "KLEE: Total Spectre found"
     echo =========================================================
     echo Running $version-loop.none.o0.s
     log=output/logs/klee/$version-loop.none.o0.log
-    timeout $timeout $whereisklee/build/bin/klee  benchmarks/spectre/bc/$version-loop.none.o0.bc --search=randomsp --enable-speculative 2> $log
+    timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative  benchmarks/spectre/bc/$version-loop.none.o0.bc 2> $log
     tail -n 1 "$log" | grep "KLEE: Total Spectre found"
     echo Running $version-loop.none.o2.s
     log=output/logs/klee/$version-loop.none.o2.log
-    timeout $timeout $whereisklee/build/bin/klee  benchmarks/spectre/bc/$version-loop.none.o2.bc --search=randomsp --enable-speculative 2> $log
+    timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative  benchmarks/spectre/bc/$version-loop.none.o2.bc 2> $log
     tail -n 1 "$log" | grep "KLEE: Total Spectre found"
     echo =========================================================
     echo
