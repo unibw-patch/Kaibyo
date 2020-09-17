2a4267b0139499b2f7218478821c907ccf0c96b4
Hernan Ponce de Leon <zeta96@gmail.com>
2020-09-17 16:42:02 +0200

    klee scripts

--- benchmarks/spectre/C2Bc.sh
+++ benchmarks/spectre/C2Bc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+whereisklee=~/git/kleespectre/klee;
+
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
-    flags="-g -c -emit-llvm -fdeclspec";
+    flags="-g -c -emit-llvm -fdeclspec -I"$whereisklee"/include/";
 
     clang-6.0 $flags -Dklee -D$version spectre.c -o bc/$version.none.o0.bc
     clang-6.0 $flags -Dklee -D$version -O2 spectre.c -o bc/$version.none.o2.bc
