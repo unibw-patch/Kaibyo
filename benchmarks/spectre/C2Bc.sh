81035e3b9032009a3f8cd2e82d0723fac78d8fe1
Hernan Ponce de Leon <zeta96@gmail.com>
2020-09-17 17:21:02 +0200

    script

--- benchmarks/spectre/C2Bc.sh
+++ benchmarks/spectre/C2Bc.sh
@@ -9,6 +9,6 @@ do
     clang-6.0 $flags -Dklee -D$version spectre.c -o bc/$version.none.o0.bc
     clang-6.0 $flags -Dklee -D$version -O2 spectre.c -o bc/$version.none.o2.bc
 
-    clang-6.0 $flags -Dklee -D$version spectre-loop.c -o bc/$version-loop.none.o0.bcw
+    clang-6.0 $flags -Dklee -D$version spectre-loop.c -o bc/$version-loop.none.o0.bc
     clang-6.0 $flags -Dklee -D$version -O2 spectre-loop.c -o bc/$version-loop.none.o2.bc
 done
--- run-klee.sh
+++ run-klee.sh
@@ -6,20 +6,20 @@ whereisklee=~/git/kleespectre/klee/
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     echo =========================================================
-    echo Running $version.none.o0.s
+    echo Running $version.none.o0.bc
     log=output/logs/klee/$version.none.o0.log
     timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative benchmarks/spectre/bc/$version.none.o0.bc 2> $log
     grep "KLEE: Total Spectre found" $log
-    echo Running $version.none.o2.s
+    echo Running $version.none.o2.bc
     log=output/logs/klee/$version.none.o2.log
     timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative benchmarks/spectre/bc/$version.none.o2.bc 2> $log
     grep "KLEE: Total Spectre found" $log
     echo =========================================================
-    echo Running $version-loop.none.o0.s
+    echo Running $version-loop.none.o0.bc
     log=output/logs/klee/$version-loop.none.o0.log
     timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative  benchmarks/spectre/bc/$version-loop.none.o0.bc 2> $log
     grep "KLEE: Total Spectre found" $log
-    echo Running $version-loop.none.o2.s
+    echo Running $version-loop.none.o2.bc
     log=output/logs/klee/$version-loop.none.o2.log
     timeout $timeout $whereisklee/build/bin/klee --search=randomsp --enable-speculative  benchmarks/spectre/bc/$version-loop.none.o2.bc 2> $log
     grep "KLEE: Total Spectre found" $log
