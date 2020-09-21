158f2b788e6dc98173fbe2ea1b4eda444f682a74
Hernan Ponce de Leon <zeta96@gmail.com>
2020-09-21 10:03:34 +0000

    csv

--- zombmc/scripts/run-klee.sh
+++ zombmc/scripts/run-klee.sh
@@ -12,52 +12,52 @@ KLEEFLAGS="--search=randomsp --enable-speculative"
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     echo =========================================================
-    
+
     name=$version.none.o0
     echo Running $name.bc
     log=/$DAT3M_HOME/output/logs/klee/$name.log
-    TIMEOUT $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
+    timeout $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
     gadget=$(grep "Spectre found" $log | wc -l)
-    echo $name, $gadget >> CSV
-    
-    NAME=$version.none.o2
+    echo $name, $gadget >> $CSV
+
+    yname=$version.none.o2
     echo Running $name.bc
     log=/$DAT3M_HOME/output/logs/klee/$name.log
-    TIMEOUT $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
+    timeout $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
     gadget=$(grep "Spectre found" $log | wc -l)
-    echo $name, $gadget >> CSV
+    echo $name, $gadget >> $CSV
 
     echo =========================================================
-    
-    NAME=$version-cloop.none.o0
-    echo Running $name.bc
-    log=/$DAT3M_HOME/output/logs/klee/$name.log
-    TIMEOUT $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
-    gadget=$(grep "Spectre found" $log | wc -l)
-    echo $name, $gadget >> CSV
 
-    NAME=$version-cloop.none.o2
+    name=$version-cloop.none.o0
     echo Running $name.bc
     log=/$DAT3M_HOME/output/logs/klee/$name.log
-    TIMEOUT $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
+    timeout $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
     gadget=$(grep "Spectre found" $log | wc -l)
-    echo $name, $gadget >> CSV
+    echo $name, $gadget >> $CSV
+
+    name=$version-cloop.none.o2
+    echo Running $name.bc
+    log=/$DAT3M_HOME/output/logs/klee/$name.log
+    timeout $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
+    gadget=$(grep "Spectre found" $log | wc -l)
+    echo $name, $gadget >> $CSV
 
     echo =========================================================
-    
-    NAME=$version-sloop.none.o0
-    echo Running $name.bc
-    log=/$DAT3M_HOME/output/logs/klee/$name.log
-    TIMEOUT $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
-    gadget=$(grep "Spectre found" $log | wc -l)
-    echo $name, $gadget >> CSV
 
-    NAME=$version-sloop.none.o2
+    name=$version-sloop.none.o0
     echo Running $name.bc
     log=/$DAT3M_HOME/output/logs/klee/$name.log
-    TIMEOUT $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
+    timeout $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
     gadget=$(grep "Spectre found" $log | wc -l)
-    echo $name, $gadget >> CSV
+    echo $name, $gadget >> $CSV
+
+    name=$version-sloop.none.o2
+    echo Running $name.bc
+    log=/$DAT3M_HOME/output/logs/klee/$name.log
+    timeout $TIMEOUT $KLEE $KLEEFLAGS $DAT3M_HOME/benchmarks/spectre/bc/$name.bc 2> $log
+    gadget=$(grep "Spectre found" $log | wc -l)
+    echo $name, $gadget >> $CSV
 
     echo =========================================================
     echo
