3794c2c1ebd972a3cc99777a6d71a67312aedba1
Hernan Ponce de Leon <zeta96@gmail.com>
2020-11-04 17:36:47 +0100

    results in the wrong order

--- zombmc/scripts/run-spectector.sh
+++ zombmc/scripts/run-spectector.sh
@@ -9,9 +9,9 @@ echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
-    for opt in o0 o2
+    for mitigation in none lfence slh
     do
-        for mitigation in none lfence slh
+        for opt in o0 o2
         do
             name=$version.$mitigation.$opt
             log=$DAT3M_HOME/output/logs/spectector/$name.log
@@ -35,9 +35,9 @@ done
 echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV-cloop
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
-    for opt in o0 o2
+    for mitigation in none lfence slh
     do
-        for mitigation in none lfence slh
+        for opt in o0 o2
         do
             name=$version-cloop.$mitigation.$opt
             log=$DAT3M_HOME/output/logs/spectector/$name.log
@@ -61,9 +61,9 @@ done
 echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV-sloop
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
-    for opt in o0 o2
+    for mitigation in none lfence slh
     do
-        for mitigation in none lfence slh
+        for opt in o0 o2
         do
             name=$version-sloop.$mitigation.$opt
             log=$DAT3M_HOME/output/logs/spectector/$name.log
--- zombmc/scripts/run-zombmc.sh
+++ zombmc/scripts/run-zombmc.sh
@@ -11,9 +11,9 @@ echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
-    for opt in o0 o2
+    for mitigation in none lfence slh
     do
-        for mitigation in none lfence slh
+        for opt in o0 o2
         do
             flag="";
             if [[ $mitigation = lfence ]]; then
@@ -44,9 +44,9 @@ echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV-c
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version-cloop
-    for opt in o0 o2
+    for mitigation in none lfence slh
     do
-        for mitigation in none lfence slh
+        for opt in o0 o2
         do
             flag="";
             if [[ $mitigation = lfence ]]; then
@@ -78,9 +78,9 @@ echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV-s
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version-sloop
-    for opt in o0 o2
+    for mitigation in none lfence slh
     do
-        for mitigation in none lfence slh
+        for opt in o0 o2
         do
             flag="";
             if [[ $mitigation = lfence ]]; then
