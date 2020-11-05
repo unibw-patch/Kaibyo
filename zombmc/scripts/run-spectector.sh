3d164c8d57373a53989ca0f3eedb5d7eb7e16300
Hernan Ponce de Leon <zeta96@gmail.com>
2020-11-05 19:58:44 +0100

    order of columns

--- zombmc/scripts/run-spectector.sh
+++ zombmc/scripts/run-spectector.sh
@@ -7,7 +7,7 @@ mkdir $LOGFOLDER
 
 CSV=$DAT3M_HOME/output/spectector.csv
 [ -e $CSV ] && rm $CSV
-echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
+echo benchmark, o0-none, o2-none, o0-lfence, o2-lfence, o0-slh, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
@@ -36,7 +36,7 @@ done
 
 CSV=$DAT3M_HOME/output/spectector-cloop.csv
 [ -e $CSV ] && rm $CSV
-echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
+echo benchmark, o0-none, o2-none, o0-lfence, o2-lfence, o0-slh, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
@@ -65,7 +65,7 @@ done
 
 CSV=$DAT3M_HOME/output/spectector-sloop.csv
 [ -e $CSV ] && rm $CSV
-echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
+echo benchmark, o0-none, o2-none, o0-lfence, o2-lfence, o0-slh, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
--- zombmc/scripts/run-zombmc.sh
+++ zombmc/scripts/run-zombmc.sh
@@ -9,7 +9,7 @@ mkdir $LOGFOLDER
 
 CSV=$DAT3M_HOME/output/zombmc.csv
 [ -e $CSV ] && rm $CSV
-echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
+echo benchmark, o0-none, o2-none, o0-lfence, o2-lfence, o0-slh, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
@@ -44,7 +44,7 @@ done
 
 CSV=$DAT3M_HOME/output/zombmc-cloop.csv
 [ -e $CSV ] && rm $CSV
-echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
+echo benchmark, o0-none, o2-none, o0-lfence, o2-lfence, o0-slh, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
@@ -80,7 +80,7 @@ done
 
 CSV=$DAT3M_HOME/output/zombmc-sloop.csv
 [ -e $CSV ] && rm $CSV
-echo benchmark, o0-none, o0-lfence, o0-slh, o2-none, o2-lfence, o2-slh >> $CSV
+echo benchmark, o0-none, o2-none, o0-lfence, o2-lfence, o0-slh, o2-slh >> $CSV
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
     line=$version
