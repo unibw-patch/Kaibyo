b9d877e8b7791b18efce43212466bdf27956a25a
Hernan Ponce de Leon <zeta96@gmail.com>
2020-12-08 19:16:04 +0100

    create ns benchmarks for klee

--- zombmc/scripts/C2Bc.sh
+++ zombmc/scripts/C2Bc.sh
@@ -4,8 +4,11 @@ DIR=$DAT3M_HOME/benchmarks/spectre
 
 for version in v01 v02 v03 v04 v05 v06 v07 v08 v09 v10 v11 v12 v13 v14 v15
 do
-    flags="-g -c -Dklee -emit-llvm -fdeclspec -I $KLEE_HOME/include/ -D"$version;
+    for mitigation in none ns
+    do
+        flags="-g -c -Dklee -emit-llvm -fdeclspec -I $KLEE_HOME/include/ -D"$version;
 
-    clang-6.0 $flags $DIR/spectre.c -o $DIR/bc/$version.none.o0.bc
-    clang-6.0 $flags -O2 $DIR/spectre.c -o $DIR/bc/$version.none.o2.bc
+        clang-6.0 $flags $DIR/spectre.c -o $DIR/bc/$version.none.o0.bc
+        clang-6.0 $flags -O2 $DIR/spectre.c -o $DIR/bc/$version.none.o2.bc
+    done
 done
