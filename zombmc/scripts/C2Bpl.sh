d77d6da90657b5a94d3b95805604d967ffa3e78f
Hernan Ponce de Leon <zeta96@gmail.com>
2021-03-02 12:16:08 +0100

    Updated script flags

--- zombmc/scripts/C2Bpl.sh
+++ zombmc/scripts/C2Bpl.sh
@@ -5,11 +5,11 @@ DIR=$DAT3M_HOME/benchmarks/spectre
 
 for version in v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 v11 v12 v13 v14 v15
 do
-    smack $DIR/spectre-pht.c $SMACKFLAGS --clang-options="-Dzombmc -D"$version" -fms-extensions -I../../include/" -bpl $DIR/bpl/spectre-pht-$version.o0.bpl
-    smack $DIR/spectre-pht.c $SMACKFLAGS --clang-options="-Dzombmc -O2 -D"$version" -fms-extensions -I../../include/" -bpl $DIR/bpl/spectre-pht-$version.o2.bpl
+    smack $DIR/spectre-pht.c $SMACKFLAGS --clang-options="-fms-extensions -Dzombmc -D"$version -bpl $DIR/bpl/spectre-pht-$version.o0.bpl
+    smack $DIR/spectre-pht.c $SMACKFLAGS --clang-options="-fms-extensions -Dzombmc -O2 -D"$version -bpl $DIR/bpl/spectre-pht-$version.o2.bpl
 done
 
 for version in v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 v11 v12 v13
 do
-    smack $DIR/spectre-stl.c $SMACKFLAGS --clang-options="-D"$version" -fms-extensions -I../../include/" -bpl $DIR/bpl/spectre-stl-$version.bpl
+    smack $DIR/spectre-stl.c $SMACKFLAGS --clang-options="-Dzombmc -D"$version -bpl $DIR/bpl/spectre-stl-$version.bpl
 done
