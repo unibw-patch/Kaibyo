42bc5550ad797a19fb68f4636873007482c05848
Hernan Ponce de Leon <zeta96@gmail.com>
2021-03-03 09:46:15 +0100

    relse timeout

--- zombmc/scripts/run-binsec.sh
+++ zombmc/scripts/run-binsec.sh
@@ -2,7 +2,7 @@
 
 TIMEOUT=60
 
-BINSECFLAGS="-relse -relse-fp 1 -sse-depth 0 -sse-load-ro-sections -sse-load-sections .got,.got.plt,.data,.plt,.data.rel.ro -fml-solver boolector -fml-solver-timeout 0 -relse-debug-level 0 -relse-paths 0 -x86-handle-seg gs -relse-timeout 3600 -relse-high-sym secretarray -relse-spectre-pht haunted -relse-spectre-dyn-pht none -relse-speculative-window 200 -sse-memory "$DAT3M_HOME/benchmarks/spectre/memory.txt
+BINSECFLAGS="-relse -relse-fp 1 -sse-depth 0 -sse-load-ro-sections -sse-load-sections .got,.got.plt,.data,.plt,.data.rel.ro -fml-solver boolector -fml-solver-timeout 0 -relse-debug-level 0 -relse-paths 0 -x86-handle-seg gs -relse-timeout "$TIMEOUT" -relse-high-sym secretarray -relse-spectre-pht haunted -relse-spectre-dyn-pht none -relse-speculative-window 200 -sse-memory "$DAT3M_HOME/benchmarks/spectre/memory.txt
 
 LOGFOLDER=$DAT3M_HOME/output/logs/binsex-$(date +%Y-%m-%d_%H:%M)
 mkdir -p $LOGFOLDER
