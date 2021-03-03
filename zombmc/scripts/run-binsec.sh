ed05dde75094c21ae0df9b86c67054d3ff5ca86b
Hernan Ponce de Leon <zeta96@gmail.com>
2021-03-03 15:17:37 +0100

    binsec script for v1

--- zombmc/scripts/run-binsec.sh
+++ zombmc/scripts/run-binsec.sh
@@ -2,14 +2,61 @@
 
 TIMEOUT=60
 
-BINSECFLAGS="-relse -relse-fp 1 -sse-depth 0 -sse-load-ro-sections -sse-load-sections .got,.got.plt,.data,.plt,.data.rel.ro -fml-solver boolector -fml-solver-timeout 0 -relse-debug-level 0 -relse-paths 0 -x86-handle-seg gs -relse-timeout "$TIMEOUT" -relse-high-sym secretarray -relse-spectre-pht haunted -relse-spectre-dyn-pht none -relse-speculative-window 200 -sse-memory "$DAT3M_HOME/benchmarks/spectre/memory.txt
+BINSECFLAGS="-relse -relse-fp 1 -sse-depth 0 -sse-load-ro-sections -sse-load-sections .got,.got.plt,.data,.plt,.data.rel.ro -fml-solver boolector -fml-solver-timeout 0 -relse-debug-level 0 -relse-paths 0 -x86-handle-seg gs -relse-timeout "$TIMEOUT" -relse-high-sym secretarray -relse-spectre-dyn-pht none -relse-speculative-window 200 -sse-memory "$DAT3M_HOME/benchmarks/spectre/memory.txt
 
-LOGFOLDER=$DAT3M_HOME/output/logs/binsex-$(date +%Y-%m-%d_%H:%M)
+LOGFOLDER=$DAT3M_HOME/output/logs/binsec-$(date +%Y-%m-%d_%H:%M)
 mkdir -p $LOGFOLDER
 
-RESULT=$DAT3M_HOME/output/binsec-results.csv
+RESULT=$DAT3M_HOME/output/binsec-spectre-v1-results.csv
 [ -e $RESULT ] && rm $RESULT
-TIMES=$DAT3M_HOME/output/binsec-times.csv
+TIMES=$DAT3M_HOME/output/binsec-spectre-v1-times.csv
+[ -e $TIMES ] && rm $TIMES
+echo benchmark, haunted, explicit >> $RESULT
+echo benchmark, haunted, explicit >> $TIMES
+
+for version in v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 v11 v12 v13 v14 v15
+do
+    rline=$version
+    tline=$version
+    for mode in haunted explicit
+    do
+        flag="-relse-spectre-stl none";
+        if [[ $mode = haunted ]]; then
+            flag+=" -relse-spectre-pht haunted";
+        fi
+        if [[ $mode = explicit ]]; then
+            flag+=" -relse-spectre-pht explicit";
+        fi
+
+        name=spectre-stl-$version.$mode
+        log=$LOGFOLDER/$name.log
+        (time timeout $TIMEOUT binsec $DAT3M_HOME/benchmarks/spectre/spectre-pht $BINSECFLAGS $flag -entrypoint victim_function_$version) > $log 2>> $log
+
+        min=$(tail -3 $log | awk 'FNR == 1 {print $2}' | awk '{split($0,a,"m"); print a[1]}')
+        sec=$(tail -3 $log | awk 'FNR == 1 {print $2}' | awk '{split($0,a,"m"); print a[2]}' | awk '{split($0,a,"."); print a[1]}')
+        ms=$(tail -3 $log  | awk 'FNR == 1 {print $2}' | awk '{split($0,a,"m"); print a[2]}' | awk '{split($0,a,"."); print a[2]}' | awk '{split($0,a,"s"); print a[1]}')
+        tline=$tline", "$((60*min+sec)).$ms
+
+        to=$(grep "Result:" $log | wc -l)
+        if [ $to -eq 0 ]; then
+            rline=$rline", \VarClock"
+            tline=$tline", "$TIMEOUT
+        else
+            unsafe=$(grep "Insecure@Status" $log | wc -l)
+            if [ $unsafe -eq 0 ]; then
+                rline=$rline", \gtick"
+            else
+                rline=$rline", \redcross"
+            fi
+        fi
+    done
+    echo $rline >> $RESULT
+    echo $tline >> $TIMES
+done
+
+RESULT=$DAT3M_HOME/output/binsec-spectre-v4-results.csv
+[ -e $RESULT ] && rm $RESULT
+TIMES=$DAT3M_HOME/output/binsec-spectre-v4-times.csv
 [ -e $TIMES ] && rm $TIMES
 echo benchmark, haunted, explicit >> $RESULT
 echo benchmark, haunted, explicit >> $TIMES
@@ -20,12 +67,12 @@ do
     tline=$version
     for mode in haunted explicit
     do
-        flag="";
+        flag="-relse-spectre-pht none";
         if [[ $mode = haunted ]]; then
-            flag+="-relse-spectre-stl haunted-ite";
+            flag+=" -relse-spectre-stl haunted-ite";
         fi
         if [[ $mode = explicit ]]; then
-            flag+="-relse-spectre-stl explicit";
+            flag+=" -relse-spectre-stl explicit";
         fi
 
         name=spectre-stl-$version.$mode
